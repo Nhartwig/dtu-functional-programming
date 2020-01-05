0c06179f18973fd1915fce4d5d7463c551e24996
Nathaniel <hartwig.n@husky.neu.edu>
2020-01-05 18:11:12 +0100

    added solutions to problems 2 and 3

--- exam-fall-2019.fs
+++ exam-fall-2019.fs
@@ -1,5 +1,5 @@
 
-// Problem 1 
+// Problem 1 (30%)
 // The five questions of this problem should be solved without the use of functions from List and Seq libraries. 
 // If you use such functions in a solution to a question, then you will get no credit for that solution. 
 
@@ -131,4 +131,207 @@ let over3 = makeOverview ([(4,10);(2,10)], m1)           // expected result: [(4
 
 
 
+// Problem 2 (15%) 
+// Conside the following F# declaration: 
 
+let rec f a xs g =
+    match xs with
+    | [] -> a
+    | x::xr -> f (g a x) xr g;;
+
+// 1.
+// Give the type of f.
+
+(* 
+    val f: 'a -> 'b list -> ('a -> 'b -> 'a) -> 'a 
+*)
+
+// 2.
+// Give a step by step evaluation (using ~>) for the expression f a [x0; x1; x2] g, where:
+// - there should be at least one step for every recursive call of f and
+// - the last expression of the evaluation does not contain f. 
+
+(*
+    f a [x0; x1; x2] g ~>  f (g a x0) [x1; x2] g
+
+    f (g a x0) [x1; x2] g ~>  f (g (g a x0) x1) [x2] g
+
+    f (g (g a x0) x1) [x2] g ~>  f (g (g (g a x0) x1) x2) [] g 
+
+    f (g (g (g a x0) x1) x2) [] g ~>  (g (g (g a x0) x1) x2)
+
+*)
+
+// 3. 
+// Give three values v1, v2 and v3 so that the expression f v1 v2 v3 has type int*string.
+
+let v1 = (0,"a")
+let v2 = [1;2;1;2]
+let v3 = (fun (x,y) z -> (x+z,y));;
+
+let ftest = f v1 v2 v3;; // expected result: (6,"a")
+
+
+// 4.
+// Declare a function h x y that gives the infinite sequence with elements x*y - i*(x-y),
+// where i = 0,1,2,3,...  . The identifier i is the index of the elements of the sequence. 
+// Give the type of h. 
+
+let h x y = seq{ yield! Seq.initInfinite(fun i -> x*y - i*(x-y))}
+let htest = h 3 5
+Seq.item 100 htest;;
+
+(*
+    val h: int -> int -> seq<int> 
+*)
+
+
+
+
+// Problem 3 (30%) 
+// We now consider the types Exp and Pat:
+
+type Exp = | Const of int  
+           | Var of string
+           | Add of Exp * Exp;;
+
+type Pat = | PConst of int
+           | PVar of string 
+           | PAdd of Pat * Pat;;
+
+// Values of type Exp are called expressions and values of type Pat are called patterns. 
+// Expressions are constructed from constants (constructor Const) and variables (constructor Var) 
+// using a constructor Add to form additions of two expressions. Patterns are constructed in a similar fashion. 
+
+// 1.
+// Declare a function vars: Pat -> string list that gives the list of those strings s that appear as PVar s in a pattern. 
+let exp1 = Add(Add(Const 2, Var "x"),Add(Var "z", Const 5))
+
+let pat1 = PAdd(PAdd(PConst 2, PVar "x"),PVar "y")
+let pat2 = PAdd(PAdd(PConst 2, PVar "x"),PAdd(PVar "z", PVar "x"))
+
+let rec vars pat = 
+    match pat with
+    | PConst i -> []
+    | PVar s -> [s]
+    | PAdd (pat1,pat2) -> vars(pat1)@vars(pat2)
+
+let vars1 = vars pat1   // expected result: ["x"; "y"]
+let vars2 = vars pat2   // expected result: ["x"; "z"; "x"]
+
+// 2.
+// A pattern p is illegal if the same string occurs multiple times as PVar s in p. Declare a function
+// legal: Pat -> bool
+// that gives value false for illegal patterns and otherwise the value true. 
+
+let legal pat =
+    let rec legal_helper strlist = 
+        match strlist with
+        | [] -> true
+        | x::xs -> if not (List.contains x xs) then (legal_helper xs) else false
+    legal_helper (vars pat)
+
+let legal1 = legal pat1     // expected result: true
+let legal2 = legal pat2     // expected result: false
+        
+
+// The types Binding and BindingList are declared as follows: 
+
+type Binding = string*Exp
+type BindingList = Binding list
+
+// A pattern p can match an expression e, and, if this is the case, a list of bindings is formed according to the following rules: 
+    // a. Pattern PConst n1 matches expression Const n2 when n1 = n2 and no binding is formed. 
+    // b. Pattern PVar s matches any expression e forming the list [(s,e)] containing one binding. 
+    // c. Pattern PAdd(p1, p2) matches expression Add(e1, e2) when p1 matches e1 and p2 matches e2,
+    // and the list of bindings formed consists of all bindings formed by matching p1 with e1 and p2 with e2. 
+    // d. Any matching of a pattern with an expression can be obtained by repeated use of the above rules a, b and c. 
+
+ // 3. 
+ // Declare a function
+ // patMatch: Pat*Exp -> BindingList option 
+ // where patMatch(p,e) = None if p does not match e and patMatch(p,e) = Some bs if 
+ // p matches e and bs is a list of bindings formed when matching p with e. 
+
+
+
+let rec patMatch (pat,exp) = 
+    let rec patMatchBool (pat,exp) = 
+        match (pat,exp) with
+        | PAdd(p1, p2) , Const n -> false
+        | PConst n, Var s -> false
+        | PAdd(p1, p2), Var s -> false
+        | PConst n, Add(e1, e2) -> false
+        | PConst n1, Const n2 -> (n1 = n2)
+        | PVar s, _ -> true
+        | PAdd(p1,p2), Add(e1,e2) -> (patMatchBool(p1,e1)&&patMatchBool(p2,e2))
+    let rec getPatMatch (pat,exp) = 
+        match (pat,exp) with
+        | PAdd(p1, p2) , Const n -> []
+        | PConst n, Var s -> []
+        | PAdd(p1, p2), Var s -> []
+        | PConst n, Add(e1, e2) -> []
+        | PConst n1, Const n2 -> []
+        | PVar s, e -> [(s, e)]
+        | PAdd(p1,p2), Add(e1,e2) -> (getPatMatch(p1,e1)@getPatMatch(p2,e2))
+    if (patMatchBool(pat,exp)) then Some (getPatMatch (pat, exp)) else None
+
+let bindinglist1 = patMatch (pat2, exp1);;    // expected result: Some [("x", Var "x"); ("z", Var "z"); ("x", Const 5)]
+
+ // We shall now consider a revision of the types Pat and Exp in order to support infix operators in general and not just addition. 
+ // In particular, we'd like to replace Add by a new constructor InfixOp so that 
+ // InfixOp(InfixOp(Const 2, "+", Var "x"), "-", Var "y"), for example, becomes a value of the revised expression type.
+
+ // Similarly, PAdd should be replaced by a new constructor PInfixOp so that, for example, 
+ // PInfixOp(PInfixOp(PConst 2, "+", PVar "x"),"-",PVar "y") becomes a value of the revised pattern type.
+ // Furthermore, the matching rule relating to the new constructors is: 
+
+ // c'. Pattern PInfixOp(p1, o, p2) matches expression InfixOp(e1, o', e2) when o = o', p1 matches e1 and p2 matches e2,
+ // and the list of bindings formed consists of all bindings formed by matching p1 with e1 and p2 with e2. 
+
+ // The following two questions concern some of the revisions needed to accommodate infix operators: 
+ // 4. 
+ // Show the revised type declarations for Pat and Exp
+
+type Exp' = | Const of int  
+            | Var of string
+            | InfixOp of Exp' * string * Exp'
+
+type Pat' = | PConst of int
+            | PVar of string 
+            | PInfixOp of Pat' * string * Pat'
+
+let exp'_1 = InfixOp(InfixOp(Const 2, "+", Var "x"), "-", Var "y")
+let pat'_1 = PInfixOp(PInfixOp(PConst 2, "+", PVar "x"),"-",PVar "y")
+
+
+ // 5.
+ // Show your revised declaration for patMatch. 
+
+let rec newPatMatch (pat,exp) = 
+    let rec patMatchBool (pat,exp) = 
+        match (pat,exp) with
+        | PInfixOp(p1,o,p2) , Const n -> false
+        | PConst n, Var s -> false
+        | PInfixOp(p1,o,p2), Var s -> false
+        | PConst n, InfixOp(e1,o',e2) -> false
+        | PConst n1, Const n2 -> (n1 = n2)
+        | PVar s, _ -> true
+        | PInfixOp(p1,o,p2), InfixOp(e1,o',e2) -> ((o=o')&&(patMatchBool(p1,e1)&&patMatchBool(p2,e2)))
+    let rec getPatMatch (pat,exp) = 
+        match (pat,exp) with
+        | PInfixOp(p1,o,p2) , Const n -> []
+        | PConst n, Var s -> []
+        | PInfixOp(p1,o,p2), Var s -> []
+        | PConst n, InfixOp(e1,o',e2) -> []
+        | PConst n1, Const n2 -> []
+        | PVar s, e -> [(s,e)]
+        | PInfixOp(p1,o,p2), InfixOp(e1,o',e2) -> (getPatMatch(p1,e1)@getPatMatch(p2,e2))
+    if (patMatchBool(pat,exp)) then Some (getPatMatch (pat, exp)) else None
+
+let bindinglist2 = newPatMatch (pat'_1, exp'_1);;   // expected result: Some [("x", Var "x"); ("y", Var "y")]
+
+
+
+// Problem 4
+// Consider the following F# declaration: 
